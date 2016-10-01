0b7f2767f23715b4cd8a207084ee6df843a7c091
vineetkhamrai <vineetkhamrai1999@gamil.com>
2016-10-01 10:10:29 +0530

    message

--- evenodd.py
+++ evenodd.py
@@ -1,5 +1,8 @@
+print("program to identify if the number is even or odd")
+
 num = int(input("Enter a number: "))
+
 if (num % 2) == 0:
-   print("{0} is Even".format(num))
+   print("The number is even")
 else:
-   print("{0} is Odd".format(num))
+   print("The number is Odd")
